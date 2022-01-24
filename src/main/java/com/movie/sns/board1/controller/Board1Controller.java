027b65c24535eb6dc8b912744f5055bb7892e37e
DESKTOP-EMIDPUF\lsw18 <lsw1873@nate.com>
2022-01-24 23:24:43 +0900

    myboard

---
+++ src/main/java/com/movie/sns/board1/controller/Board1Controller.java
@@ -0,0 +1,17 @@
+package com.movie.sns.board1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/board1/*")
+public class Board1Controller {
+
+	@RequestMapping(value="myBoard", method=RequestMethod.GET)
+	public String myboard() {
+		
+		return "board1/myBoard";
+	}
+	
+}
