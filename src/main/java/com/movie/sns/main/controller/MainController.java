1ea41627860fe0c8f09afd4a17bd622558770a34
dong-hoon <ehdgns874@gmail.com>
2022-01-27 14:03:03 +0900

    유동훈커밋

--- src/main/java/com/movie/sns/main/controller/MainController.java
+++ src/main/java/com/movie/sns/main/controller/MainController.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import com.movie.sns.member.model.vo.Member;
 
 @Controller
+@SessionAttributes({"loginMember"})
 public class MainController {
 
 
 	
 	@RequestMapping(value="main", method=RequestMethod.GET)
-	public String mainPageForward(Model model , @ModelAttribute("loginMember")Member loginMember) {
+	public String mainPageForward() {
 		
 		return "main/main";
 	}
