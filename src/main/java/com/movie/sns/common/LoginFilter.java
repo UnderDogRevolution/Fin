bc1f10f1ba9ded7f9df15d8a1013d4213a9f8eb4
DESKTOP-L00EK8F\user <trust9211@naver.com>
2022-01-28 00:00:31 +0900

    필터 중간 완성

--- src/main/java/com/movie/sns/common/LoginFilter.java
+++ src/main/java/com/movie/sns/common/LoginFilter.java
@@ -1,6 +1,7 @@
 package com.movie.sns.common;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -16,13 +17,9 @@ import javax.servlet.http.HttpSession;
 import com.movie.sns.member.model.vo.Member;
 
 
-// 로그인 하면 접근 못하도록하는 url
-
-
- 
-
 @WebFilter(	filterName = "loginFilter" ,
-			urlPatterns = { "/member/login", "/member/signUp" } )
+			urlPatterns = { "/member/*", "/chat/*" } )
+// member/* , 글쓰기,수정,삭제,북마크 등등   chat/* 
 public class LoginFilter implements Filter{
 
 	public void init(FilterConfig filterConfig) throws ServletException {
@@ -36,23 +33,69 @@ public class LoginFilter implements Filter{
 		HttpServletRequest req = (HttpServletRequest)request;
 		HttpServletResponse resp = (HttpServletResponse)response;
 		
+		String uri = req.getRequestURI();
+		String contextPath = req.getContextPath();
+		
+		String[] arr = uri.substring( (contextPath +"/").length()).split("/");	
+		
 		HttpSession session = req.getSession();
 		Member loginMember = (Member)session.getAttribute("loginMember");
 		
-		// 로그인이 되어있는 경우
-		if(loginMember != null) {
 			
-			// 피드 홈으로 보내도록 주소 변경하기
-			System.out.println("[로그인 필터 적용] 피드 페이지로 이동");
-			resp.sendRedirect(req.getContextPath()+"/main");
+		// ---------------- 비회원인 경우 ----------------
+		if(loginMember == null) {
 			
+			// 비회원 + /member/* 인 경우
+			if(arr[0].equals("member")) {
+				
+				// 비회원 + /member/login , /member/signUp , member/findPw 인 경우
+				if(arr[1].equals("login") || arr[1].equals("signUp") ||arr[1].equals("findPw")) {
+					chain.doFilter(request, response);
+				}else {
+					// 그 외에는 모두 로그인페이지로
+					System.out.println("[필터] : 잘못된 접근입니다. 로그인페이지로 이동합니다.");
+					resp.sendRedirect(req.getContextPath() + "/member/login");
+				}
+				
+			// 비회원 + /chat/* 인 경우
+			}else if(arr[0].equals("chat")) {
+				
+				System.out.println("[필터] : 잘못된 접근입니다. 로그인페이지로 이동합니다.");
+				resp.sendRedirect(req.getContextPath() + "/member/login");
+				
+			// 비회원 + 나머지 페이지
+			}else {
+				chain.doFilter(request, response);
+			}
+			
+			
+			
+		// ---------------- 회원인 경우 ----------------
 		}else {
 			
-			chain.doFilter(request, response);
+			if(arr[0].equals("member")) {
+				
+				// 회원 + /member/login , /member/signUp , member/findPw 인 경우
+				if(arr[1].equals("login") || arr[1].equals("signUp") ||arr[1].equals("findPw")) {
+					resp.sendRedirect(req.getContextPath());
+					// resp.sendRedirect(req.getContextPath()+"/main");
+				}else {
+					chain.doFilter(request, response);
+				}
+				
+			}else {
+				chain.doFilter(request, response);
+			}
 			
 		}
 		
-		
+//			// 피드 홈으로 보내도록 주소 변경하기
+//			System.out.println("[로그인 필터 적용] 피드 페이지로 이동");
+//			resp.sendRedirect(req.getContextPath()+"/main");
+//			
+//			// 로그인 페이지로 보내버리기 + sweet alert 가능하면 사용
+//			System.out.println("[로그인 필터 적용] 로그인 페이지로 이동");
+//			resp.sendRedirect(req.getContextPath()+"/member/login");
 		
 	}
 
