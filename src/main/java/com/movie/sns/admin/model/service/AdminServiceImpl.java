c9e23a2e516efc9cc264860706686f02ed32c128
dong-hoon <ehdgns874@gmail.com>
2022-02-07 04:42:25 +0900

    관리자로그인기능 메인페이지 회원조회 페이지

---
+++ src/main/java/com/movie/sns/admin/model/service/AdminServiceImpl.java
@@ -0,0 +1,31 @@
+package com.movie.sns.admin.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.movie.sns.admin.model.dao.AdminDAO;
+import com.movie.sns.admin.model.vo.Admin;
+
+
+
+
+
+
+@Service
+public class AdminServiceImpl implements AdminService{
+
+	@Autowired
+	private AdminDAO dao;
+
+	/** 관리자 로그인
+	 *
+	 */
+	@Override
+	public Admin adminLogin(Admin member) {
+
+			
+		return dao.adminLogin(member);
+	}
+	
+	
+}
