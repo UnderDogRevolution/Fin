54db6df6ac66e4b8148c22b0e0b04d46de746bc2
dong-hoon <ehdgns874@gmail.com>
2022-02-08 11:47:36 +0900

    관리자 페이지 css 수정 및 상세페이지 조회 끝

--- src/main/java/com/movie/sns/admin/controller/AdminController.java
+++ src/main/java/com/movie/sns/admin/controller/AdminController.java
@@ -23,7 +23,7 @@ import com.movie.sns.member.model.vo.Member;
 
 @Controller
 @RequestMapping("/admin/*")
-@SessionAttributes({"loginMember"}) 
+@SessionAttributes({"adminLoginMember"}) 
 public class AdminController {
 
 	@Autowired
@@ -45,7 +45,7 @@ public class AdminController {
 
 		if (loginMember != null) {
 
-			model.addAttribute("loginMember", loginMember);
+			model.addAttribute("adminLoginMember", loginMember);
 
 			path = "admin/adminMain";
 
