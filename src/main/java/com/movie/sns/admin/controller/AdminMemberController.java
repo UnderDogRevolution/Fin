881cdc920c1efc7eaec5734eabe9697448278ef1
KiteJun92 <trust9211@naver.com>
2022-02-08 11:11:12 +0900

    1. 로그파일 생성안되게 수정 2. loginMember 로그에 안찍히게 수정 (Admin과 충돌나서 오류 발생)

--- src/main/java/com/movie/sns/admin/controller/AdminMemberController.java
+++ src/main/java/com/movie/sns/admin/controller/AdminMemberController.java
@@ -4,17 +4,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.movie.sns.admin.model.service.AdminMemberService;
 import com.movie.sns.admin.model.vo.Admin;
+import com.movie.sns.admin.model.vo.AdminMemberSearch;
+import com.movie.sns.admin.model.vo.Pagination;
 import com.movie.sns.member.model.vo.Member;
 
 @Controller
-@RequestMapping("/admin/adminMember/*")
+@RequestMapping("/admin/member/*")
 @SessionAttributes({"loginMember"})
 public class AdminMemberController {
 	
@@ -25,27 +29,25 @@ public class AdminMemberController {
 
 	// 회원정보조회
 	@RequestMapping(value = "list", method = RequestMethod.GET)
-	public String memberBoard(Admin member) {
+	public String memberBoard(	Admin member,
+								@RequestParam(value="cp", required=false, defaultValue="1") int cp,
+								Model model /*, AdminMemberSearch search */) 
+	{
+		// search 추가 예정
 		
+		Pagination pagination = null;
+		List<Member> memberList = null;
 		
+		// 검색 조건이 따로 없는 경우
+		pagination = service.getPagination(cp);
 		
+		memberList = service.selectMemberList(pagination);
 		
 		
 		return "admin/adminMember";
 	}
 	
 	
-	// 멤버 리스트를 조회해서 member를 json으로 반환
-	@RequestMapping(value="selectMemberList", method=RequestMethod.GET)
-	@ResponseBody
-	public List<Member> selectMemberList() {
-		
-		
-		
-		return null;
-		
-		
-	}
 
 	
 	
--- src/main/java/com/movie/sns/admin/model/service/AdminMemberServiceImpl.java
+++ src/main/java/com/movie/sns/admin/model/service/AdminMemberServiceImpl.java
@@ -1,9 +1,13 @@
 package com.movie.sns.admin.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.movie.sns.admin.model.dao.AdminMemberDAO;
+import com.movie.sns.admin.model.vo.Pagination;
+import com.movie.sns.member.model.vo.Member;
 
 @Service
 public class AdminMemberServiceImpl implements AdminMemberService{
@@ -11,7 +15,17 @@ public class AdminMemberServiceImpl implements AdminMemberService{
 	
 	@Autowired
 	private AdminMemberDAO dao;
+
 	
+	@Override
+	public Pagination getPagination(int cp) {
+		
+		int listCount = dao.getListCount(); 
+		
+		return null;
+	}
+
+
 	
 	
 	
