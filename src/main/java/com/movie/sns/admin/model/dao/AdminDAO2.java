171e60cf23930315de53fc5ccc0cf9c91c7c3320
LJB <dlwnsqja1122@gmail.com>
2022-02-08 18:48:01 +0900

    문의글 추가

---
+++ src/main/java/com/movie/sns/admin/model/dao/AdminDAO2.java
@@ -0,0 +1,54 @@
+package com.movie.sns.admin.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.movie.sns.admin.model.vo.Admin;
+import com.movie.sns.admin.model.vo.AdminAsk;
+import com.movie.sns.admin.model.vo.Pagination;
+import com.movie.sns.member.model.vo.Member;
+@Repository
+public class AdminDAO2 {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	/** 전체 게시글 수 count
+	 * @return listCount
+	 */
+	public int getListCount() {
+		return sqlSession.selectOne("adminMapper3.getListCount");
+	}
+	
+	
+	
+	
+	/** 10페이지 범위 문의글 목록 조회
+	 * @param pagination
+	 * @return
+	 */
+	public List<AdminAsk> selectAskList(Pagination pagination) {
+
+		int offset = (pagination.getCurrentPage() - 1) * pagination.getLimit() ;
+		int limit = pagination.getLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, limit);
+		
+		return sqlSession.selectList("adminMapper3.selectAskList", null, rowBounds);
+	}
+
+
+
+
+	/** 문의글 상세 조회
+	 * @param askNo
+	 * @return
+	 */
+	public AdminAsk selectAsk(int askNo) {
+		return sqlSession.selectOne("adminMapper3.selectAsk", askNo);
+	}
+
+}
