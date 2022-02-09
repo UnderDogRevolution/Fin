ae07b2aeae638bd239481df38996fd6455ae3a01
KiteJun92 <trust9211@naver.com>
2022-02-09 14:40:22 +0900

    1.관리지 페이지 필터 완성

--- src/main/java/com/movie/sns/filter/LoginFilter.java
+++ src/main/java/com/movie/sns/filter/LoginFilter.java
@@ -18,7 +18,7 @@ import com.movie.sns.member.model.vo.Member;
 
 
 @WebFilter(	filterName = "loginFilter" ,
-			urlPatterns = { "/member/*", "/chat/*" } )
+			urlPatterns = { "/member/*", "/chat/*", "/admin/*" } )
 // member/* , 글쓰기,수정,삭제,북마크 등등   chat/* 
 // main/* , admin/* 추가 예정 
 public class LoginFilter implements Filter{
@@ -44,60 +44,112 @@ public class LoginFilter implements Filter{
 		HttpSession session = req.getSession();
 		Member loginMember = (Member)session.getAttribute("loginMember");
 		
-		// 관리자 필터
 		
-		// ---------------- 비로그인  ----------------
-		if(loginMember == null) {
+		// 주소 값이 fin/ * / * 인 경우
+		if(arr.length > 1) {
 			
-			// 비회원 + /member/* 인 경우
-			if(arr[0].equals("member")) {
+			// ---------------- 비로그인  ----------------
+			if(loginMember == null) {
 				
-				// 회원만 이용 가능한 주소 값
-				if(		arr[1].equals("myPage") || 
-						arr[1].equals("update") || 
-						arr[1].equals("updatePw") || 
-						arr[1].equals("secession") || 
-						arr[1].equals("ask") 
-					) {
+				// 비회원 + /member/* 인 경우
+				if(arr[0].equals("member")) {
+					
+					// 회원만 이용 가능한 주소 값
+					if(		arr[1].equals("myPage") || 
+							arr[1].equals("update") || 
+							arr[1].equals("updatePw") || 
+							arr[1].equals("secession") || 
+							arr[1].equals("ask") 
+							) {
+						System.out.println("[필터] : 잘못된 접근입니다. 로그인페이지로 이동합니다.");
+						resp.sendRedirect(req.getContextPath() + "/member/login");
+					}else {
+						chain.doFilter(request, response);
+					}
+					
+					
+					
+				}
+				// 비회원 + /chat/* 인 경우
+				else if(arr[0].equals("chat")) {
+					
 					System.out.println("[필터] : 잘못된 접근입니다. 로그인페이지로 이동합니다.");
 					resp.sendRedirect(req.getContextPath() + "/member/login");
-				}else {
+					
+				}
+				// 비회원 + admin/* 페이지
+				else if(arr[0].equals("admin")) {
+					
+					if(		arr[1].equals("member") || 
+							arr[1].equals("ask") || 
+							arr[1].equals("post") || 
+							arr[1].equals("report") || 
+							arr[1].equals("statistics") 
+							) {
+						System.out.println("[필터] : 잘못된 접근입니다. 관리자 로그인페이지로 이동합니다.");
+						resp.sendRedirect(req.getContextPath() + "/admin/");
+						
+					}else {
+						chain.doFilter(request, response);
+					}
+					
+				}
+				// 비회원 + 나머지 페이지
+				else {
 					chain.doFilter(request, response);
 				}
 				
 				
 				
-			// 비회원 + /chat/* 인 경우
-			}else if(arr[0].equals("chat")) {
-				
-				System.out.println("[필터] : 잘못된 접근입니다. 로그인페이지로 이동합니다.");
-				resp.sendRedirect(req.getContextPath() + "/member/login");
-				
-			// 비회원 + 나머지 페이지
+				// ---------------- 로그인 ----------------
 			}else {
-				chain.doFilter(request, response);
+				
+				if(arr[0].equals("member")) {
+					
+					// 회원 + /member/login , /member/signUp , member/findPw 인 경우
+					if(arr[1].equals("login") || arr[1].equals("signUp") ||arr[1].equals("findPw") || arr[1].equals("resetPw") ) {
+						resp.sendRedirect(req.getContextPath());
+						// resp.sendRedirect(req.getContextPath()+"/main");
+					}else {
+						chain.doFilter(request, response);
+					}
+					
+				}
+				else if(arr[0].equals("admin")) {
+					
+					if(loginMember.getMemberGradeCode() == 100) {
+						
+						// 일반 회원인 경우
+						if(		arr[1].equals("member") || 
+								arr[1].equals("ask") || 
+								arr[1].equals("post") || 
+								arr[1].equals("report") || 
+								arr[1].equals("statistics") 
+								) {
+							System.out.println("[필터] : 잘못된 접근입니다. 관리자 로그인페이지로 이동합니다.");
+							resp.sendRedirect(req.getContextPath() + "/admin/");
+						}else {
+							chain.doFilter(request, response);
+						}
+						
+					}else if(loginMember.getMemberGradeCode() == 101) {
+						
+						// 관리자인 경우
+						chain.doFilter(request, response);
+						
+					}
+					
+				}
+				else {
+					chain.doFilter(request, response);
+				}
+				
 			}
 			
-			
-			
-		// ---------------- 로그인 ----------------
 		}else {
-			
-			if(arr[0].equals("member")) {
-				
-				// 회원 + /member/login , /member/signUp , member/findPw 인 경우
-				if(arr[1].equals("login") || arr[1].equals("signUp") ||arr[1].equals("findPw") || arr[1].equals("resetPw") ) {
-					resp.sendRedirect(req.getContextPath());
-					// resp.sendRedirect(req.getContextPath()+"/main");
-				}else {
-					chain.doFilter(request, response);
-				}
-				
-			}else {
-				chain.doFilter(request, response);
-			}
-			
+			chain.doFilter(request, response);
 		}
+		
 		
 	}
 
